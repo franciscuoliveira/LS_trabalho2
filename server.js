665e083cdae4066b76a4772b027ff5f2114d5876
franciscuoliveira <58727899+franciscuoliveira@users.noreply.github.com>
2022-01-18 19:51:40 +0000

    compra

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const express = require('express')
-const logger = require('morgan')
-const errorhandler = require('errorhandler')
-const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
+const express = require('express');
+const logger = require('morgan');
+const errorhandler = require('errorhandler');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 require('dotenv/config');
-let app = express()
+let app = express();
 app.use('/public', express.static('files'));
 var formDataParser = bodyParser.urlencoded({ extended: true });
 app.use(bodyParser.json())
@@ -13,6 +13,7 @@ app.use(errorhandler())
 
 //Imports Routes
 const postsRoute = require('./routes/posts');
+const { stringify } = require('nodemon/lib/utils');
 
 app.use('/posts', postsRoute);
 
@@ -52,4 +53,29 @@ app.get('/alterarDados', (req, res) => {
 })
 app.get('/relatorios', (req, res) => {
     res.render('pages/relatorios')
+})
+
+//compra
+//create data schema
+const compraSchema = {
+    matricula: String,
+    marca: String, 
+    modelo: String,
+    tipo: String,
+    ano: String,
+    preco: String
+}
+const Compra = mongoose.model("Car", compraSchema);
+
+app.post("/compra", function(req,res){
+    let newcompra = new Compra({
+        matricula: req.body.matricula,
+        marca: req.body.marca, 
+        modelo: req.body.modelo,
+        tipo: req.body.tipo,
+        ano: req.body.ano,
+        preco: req.body.precoCompra
+    })
+    newcompra.save();
+    res.redirect('/compra');
 })
